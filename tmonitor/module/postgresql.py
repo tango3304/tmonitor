108f9c7e13cfa25ea230bab6cf4a48e54bab3c3b
tango3304 <94199810+tango3304@users.noreply.github.com>
2023-11-19 12:14:21 +0900

    Update postgresql.py
    Add re.compile Module
    Updata PostgreSQLselect Class

--- tmonitor/module/postgresql.py
+++ tmonitor/module/postgresql.py
@@ -3,6 +3,7 @@ import psycopg2
 from datetime import datetime, timedelta
 from traceback import print_tb, format_exception_only
 from sys import exc_info, exit
+from re import compile
 
 class PostgreSQLConnection:       
 	def connection():
@@ -48,17 +49,31 @@ class PostgreSQLinsert:
 
 
 class PostgreSQLselect:
-	def select():
+	def select(ipaddress, year, month, day):
+	# Check IPaddress [IPアドレス確認]
+	#    0-99: [1-9]?[0-9]
+	# 100-199: 1[0-9]{2}(1[0-9][0-9])
+	# 200-249: 2[0-4][0-9]
+	# 250-255: 25[0-5]
+		check_ipaddress = compile(r'^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$')
+		if check_ipaddress.fullmatch(ipaddress) == None:
+			print(f"\nInvalid IPaddress [無効なIPアドレスです]")
+			exit(1)
+		
+		search_date = f'{year}-{month}-{day}'
 		connection = PostgreSQLConnection.connection()
 
 	# Record Reference [レコード参照]
 		with connection as con:
 			with con.cursor() as con_cur:
-				select_sql = 'SELECT * FROM alive'
+				select_sql = f"""
+					SELECT to_char(timestamp, 'yyyy/mm/dd') as date, to_char(timestamp, 'HH24:MI')as time, status_flag, target
+					FROM alive WHERE CAST(timestamp as date)= '{search_date}' AND target= '{ipaddress}';
+				"""
 				con_cur.execute(select_sql)
 				select_values = con_cur.fetchall()
-				for ram in select_values:
-					print(ram)
+
+				return select_values
 
 
 class PostgreSQLdelete:
